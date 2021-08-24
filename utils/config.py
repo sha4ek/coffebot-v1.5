ee6f848507986ea119538720558ce6f7871d7699
Sha4ek <69022471+Sha4ek@users.noreply.github.com>
2021-08-24 16:36:51 +1000

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,10 +1,12 @@
 import os
 
-BotPrefix = 'c.'
-BotToken = os.environ.get('AppToken')
-BotBasicColor = 0xdc9f56
-BotErrorColor = 0xcf080f
+BotPrefix = 'c.' # префикс бота
+BotToken = os.environ.get('AppToken') # токен бота
+BotBasicColor = 0xdc9f56 # обычный цвет рамки
+BotErrorColor = 0xcf080f # цвет рамки при ошибке
+BotNormalColor = 0x80d7a4 # цвет рамки при верном решении
 
-def BotPostfix(num:int, end_1:str='год', end_2:str='года', end_3:str='лет'):
+
+def BotPostfix(num: int, end_1: str='год', end_2: str='года', end_3: str='лет'): # функция постфикса
     num = num % 10 if num > 20 else num
     return end_1 if num == 1 else end_2 if 1 < num < 5 else end_3
