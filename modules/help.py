21193eab1022b16a9744469f2ac58dc1c61979e5
Sha4ek <69022471+Sha4ek@users.noreply.github.com>
2021-08-31 14:30:06 +1000

    Update help.py

--- modules/help.py
+++ modules/help.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from Cybernator import Paginator # импортируем подмодуль для страниц (pip install Cybernator)
-from utils.config import BotBasicColor, BotPrefix # импортируем конфиг бота
+from utils.config import BotSettings # импортируем конфиг бота
 
 
 class Help(commands.Cog): # создаём класс модуля команды помощи
@@ -10,17 +10,36 @@ class Help(commands.Cog): # создаём класс модуля команд�
 
 
     @commands.command()
+    @commands.cooldown(rate=1, per=4.0, type=commands.BucketType.user)
+    @commands.bot_has_permissions(send_messages=True, embed_links=True, read_message_history=True,
+        add_reactions=True, manage_messages=True)
     async def help(self, ctx): # создаём команду помощи
-
         emb1 = discord.Embed(title='Информационные команды:',
-            description=f'**:chart_with_upwards_trend: {BotPrefix}stats** - немного статистики бота',
-            color=BotBasicColor)
+            description=f'**:chart_with_upwards_trend: {BotSettings["Bot"]["Prefix"][0]}stats** - немного статистики бота\n'
+                        f'**:scroll: {BotSettings["Bot"]["Prefix"][0]}user [участник]** - информация об участнике\n'
+                        f'**:page_facing_up: {BotSettings["Bot"]["Prefix"][0]}channel [канал]** - информация о канале\n'
+                        f'**:notes: {BotSettings["Bot"]["Prefix"][0]}lyrics <песня>** - поиск текста песни\n',
+            color=BotSettings['Bot']['BasicColor'])
         emb2 = discord.Embed(title='Фановые команды:',
-            description=f'**:video_game: {BotPrefix}activity [ytt/chess/poker]** - активности в голосовом канале') # делаем эмбеды
-        embs = [emb1, emb2] # объединяем эмбеды
+            description=f'**:video_game: {BotSettings["Bot"]["Prefix"][0]}activity [ytt/chess/poker]** - активности в голосовом канале\n'
+                        f'**:cat: {BotSettings["Bot"]["Prefix"][0]}cat** - картинки котиков') # делаем эмбеды
+        emb3 = discord.Embed(title='Модерационые команды:',
+            description=f'**:broom: {BotSettings["Bot"]["Prefix"][0]}clear <кол-во>** - очистка чата\n'
+                        f'**:heavy_plus_sign: {BotSettings["Bot"]["Prefix"][0]}addrole <участник> <роль>** - добавление роли участнику\n'
+                        f'**:hammer: {BotSettings["Bot"]["Prefix"][0]}ban <участник> [причина]** - бан участника\n'
+                        f'**:mute: {BotSettings["Bot"]["Prefix"][0]}mute <участник>** - мьют участника\n'
+                        f'**:sound: {BotSettings["Bot"]["Prefix"][0]}unmute <участник>** - размьют участника\n')
+        emb4 = discord.Embed(title='Команды разработчика:',
+            description=f'**:grey_exclamation: {BotSettings["Bot"]["Prefix"][0]}modules [load/unload/reload]** - управление модулями бота\n'
+                        f'**:gear: {BotSettings["Bot"]["Prefix"][0]}jsk <функция> <код>** - исполнение кода ботом вне редактора')
+        
+        if ctx.author.id == BotSettings['Bot']['OwnerID']: # проверка на создателя бота
+            embs = [emb1, emb2, emb3, emb4] # объединяем эмбеды
+        else:
+            embs = [emb1, emb2, emb3] # объединяем эмбеды
         message = await ctx.send(embed=emb1)
         pages = Paginator(self.Bot, message, embeds=embs, timeout=60, only=ctx.author, footer=False,
-            color=BotBasicColor, use_remove_reaction=False) # выставляем настройки страниц (Документация: https://github.com/RuCybernetic/Cybernator/blob/master/README_Ru.md)
+            color=BotSettings['Bot']['BasicColor']) # выставляем настройки страниц (Документация: https://github.com/RuCybernetic/Cybernator/blob/master/README_Ru.md)
         await pages.start() # запускаем страницы
 
 
