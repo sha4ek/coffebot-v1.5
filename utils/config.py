6d2e64155d7dd8febca261d8ae876607825884d7
Sha4ek <69022471+Sha4ek@users.noreply.github.com>
2021-09-05 17:19:16 +1000

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,62 +1,19 @@
 import os, time
 
 
-def BotPostfix(num: int, end_1: str='год', end_2: str='года', end_3: str='лет'): # функция постфикса
-    num = num % 10 if num > 20 else num
-    return end_1 if num == 1 else end_2 if 1 < num < 5 else end_3
-
-
-def BotUptime():
-    seconds = 0
-    minutes = 0
-    hours = 0
-    days = 0 # стандартные переменные времени
-
-    while True: # создаём цикл
-        seconds += 1 
-
-        if seconds == 60:
-            minutes += 1
-            seconds = 0
-
-        if minutes == 60:
-            hours += 1
-            minutes = 0
-
-        if hours == 24:
-            days += 1
-            hours = 0
-
-        if seconds != 0:
-            file = open('uptime.txt', 'w')
-            file.write(f'{seconds} {BotPostfix(seconds, "секунда", "секунды", "секунд")}')
-            file.close()
-
-        if minutes != 0:
-            file = open('uptime.txt', 'w')
-            file.write(f'{minutes} {BotPostfix(minutes, "минута", "минуты", "минут")} {seconds} {BotPostfix(seconds, "секунда", "секунды", "секунд")}')
-            file.close()
-
-        if hours != 0:
-            file = open('uptime.txt', 'w')
-            file.write(f'{hours} {BotPostfix(hours, "час", "часа", "часов")} {minutes} {BotPostfix(minutes, "минута", "минуты", "минут")} {seconds} {BotPostfix(seconds, "секунда", "секунды", "секунд")}')
-            file.close()
-
-        if days != 0:
-            file = open('uptime.txt', 'w')
-            file.write(f'{days} {BotPostfix(days, "день", "дня", "дней")} {hours} {BotPostfix(hours, "час", "часа", "часов")} {minutes} {BotPostfix(minutes, "минута", "минуты", "минут")} {seconds} {BotPostfix(seconds, "секунда", "секунды", "секунд")}')
-            file.close()
-        time.sleep(1)
-
-
 BotSettings = {
     'Bot': {
         'Token': os.environ.get('AppToken'), # токен бота
-        'Prefix': ['c.', 'C.'], # префикс бота
+        'MainPrefix': 'c.', # префикс бота
         'BasicColor': 0xdc9f56, # обычный цвет рамки
         'ErrorColor': 0xcf080f, # цвет рамки при ошибке
         'NormalColor': 0x80d7a4, # цвет рамки при верном решении
-        'OwnerID': 546502974499717122 # ID разработчика бота
+        'OwnerID': 546502974499717122, # ID разработчика бота
+        'ErrorsLogChannel': 880968020980273173,
+        'GuildsLogChannel': 883974406446149633
+    },
+    'Mongo': {
+        'Collection': MongoClient(os.environ.get('MongoToken')).Coffeebase
     },
     'Permissions': {
         'view_channel': 'просматривать каналы',
@@ -115,3 +72,70 @@ BotSettings = {
     },
     'BoticordToken': os.environ.get('BoticordToken')
 }
+
+
+def BotPostfix(num: int, end_1: str='год', end_2: str='года', end_3: str='лет'): # функция постфикса
+    num = num % 10 if num > 20 else num
+    return end_1 if num == 1 else end_2 if 1 < num < 5 else end_3
+
+
+def BotUptime():
+    seconds = 0
+    minutes = 0
+    hours = 0
+    days = 0 # стандартные переменные времени
+
+    while True: # создаём цикл
+        seconds += 1 
+
+        if seconds == 60:
+            minutes += 1
+            seconds = 0
+
+        if minutes == 60:
+            hours += 1
+            minutes = 0
+
+        if hours == 24:
+            days += 1
+            hours = 0
+
+        if seconds != 0:
+            file = open('uptime.txt', 'w')
+            file.write(f'{seconds} {BotPostfix(seconds, "секунда", "секунды", "секунд")}')
+            file.close()
+
+        if minutes != 0:
+            file = open('uptime.txt', 'w')
+            file.write(f'{minutes} {BotPostfix(minutes, "минута", "минуты", "минут")} {seconds} {BotPostfix(seconds, "секунда", "секунды", "секунд")}')
+            file.close()
+
+        if hours != 0:
+            file = open('uptime.txt', 'w')
+            file.write(f'{hours} {BotPostfix(hours, "час", "часа", "часов")} {minutes} {BotPostfix(minutes, "минута", "минуты", "минут")} {seconds} {BotPostfix(seconds, "секунда", "секунды", "секунд")}')
+            file.close()
+
+        if days != 0:
+            file = open('uptime.txt', 'w')
+            file.write(f'{days} {BotPostfix(days, "день", "дня", "дней")} {hours} {BotPostfix(hours, "час", "часа", "часов")} {minutes} {BotPostfix(minutes, "минута", "минуты", "минут")} {seconds} {BotPostfix(seconds, "секунда", "секунды", "секунд")}')
+            file.close()
+        time.sleep(1)
+        
+        
+def GetPrefix(Bot, message):
+    collection = BotSettings['Mongo']['Collection'].custom_prefix
+
+    if collection.count_documents({'guild_id': message.guild.id}) == 0: # проверка на присутствие сервера в базе
+        collection.insert_one({
+            'guild_name': message.guild.name,
+            'guild_id': message.guild.id,
+            'guild_owner_name': f'{message.guild.owner.name}#{message.guild.owner.discriminator}',
+            'guild_owner_id': message.guild.owner.id,
+            'guild_prefix': BotSettings['Bot']['MainPrefix']
+            })
+        
+        prefix = collection.find_one({'guild_id': message.guild.id})
+        return prefix['guild_prefix']
+    else:
+        prefix = collection.find_one({'guild_id': message.guild.id}) # если есть то берем его оттуда
+        return prefix['guild_prefix']
