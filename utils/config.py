f827982db0ed7696584dfb43e68379890ad27c19
Sha4ek <69022471+Sha4ek@users.noreply.github.com>
2021-10-10 10:57:01 +1000

    Вроде как исправлена ошибка Монги

--- utils/config.py
+++ utils/config.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 
 MongoSettings = {
     # База данных серверов
-    'GuildsData': MongoClient(f'mongodb+srv://{os.environ.get('MongoKey')}/TestCoffeebase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE').Coffeebase.guilds_data
+    'GuildsData': MongoClient(f'mongodb+srv://{os.environ.get("MongoKey")}/TestCoffeebase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE').Coffeebase.guilds_data
 }
 
 
