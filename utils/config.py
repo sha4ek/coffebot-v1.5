09ebbd1aa8bcea5fadeb55c3cadef866b3e3bdf4
Sha4ek <69022471+Sha4ek@users.noreply.github.com>
2021-10-10 10:23:18 +1000

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,142 +1,35 @@
-import os, time
 from pymongo import MongoClient
 
 
-BotSettings = {
-    'Bot': {
-        'Token': os.environ.get('AppToken'), # токен бота
-        'MainPrefix': 'c.', # префикс бота
-        'BasicColor': 0xdc9f56, # обычный цвет рамки
-        'ErrorColor': 0xcf080f, # цвет рамки при ошибке
-        'NormalColor': 0x80d7a4, # цвет рамки при верном решении
-        'OwnerID': 546502974499717122, # ID разработчика бота
-        'ErrorsLogChannel': 896215788455886849,
-        'GuildsLogChannel': 896215771347320872
-    },
-    'Mongo': {
-        'Collection': MongoClient(os.environ.get('MongoToken')).Coffeebase
-    },
-    'Permissions': {
-        'view_channel': 'просматривать каналы',
-        'manage_channels': 'управлять каналами',
-        'manage_roles': 'управлять ролями',
-        'manage_emojis_and_stickers': 'управлять эмодзи и стикерами',
-        'view_audit_log': 'просматривать журнала аудита',
-        'view_guild_insinghts': 'просмотр аналитики сервера',
-        'manage_webhooks': 'управлять вебхуками (webhooks)',
-        'manage_guild': 'управлять сервером',
-        'create_instant_invite': 'создание приглашения',
-        'change_nickname': 'изменить никнейм',
-        'manage_nicknames': 'управлять никнеймами',
-        'kick_members': 'выгонять участников',
-        'ban_members': 'банить участников',
-        'send_messages': 'отправлять сообщения',
-        'send_messages_in_threads': 'send messages in threads (coming soon)',
-        'use_public_threads': 'use public threads',
-        'use_private_threads': 'use private threads',
-        'embed_links': 'встраивать ссылки',
-        'attach_files': 'прикреплять файлы',
-        'add_reactions': 'добавлять реакции',
-        'use_external_emojis': 'использовать внешние эмодзи',
-        'use_external_stickers': 'использовать внешние стикеры',
-        'mention_everyone': 'упоминание @everyone, @here и всех ролей',
-        'manage_messages': 'управлять сообщениями',
-        'manage_threads': 'управление ветками',
-        'read_message_history': 'читать историю сообщений',
-        'send_tts_messages': 'отправка сообщений text-to-speech',
-        'use_application_commands': 'use application commands',
-        'connect': 'подключаться',
-        'speak': 'говорить',
-        'video': 'видео',
-        'use_voice_activity': 'использовать режим активации по голосу',
-        'priority_speaker': 'приоритетный режим',
-        'mute_members': 'отключать участникам микрофон',
-        'deafen_members': 'отключать участникам звук',
-        'move_members': 'перемещать участников',
-        'request_to_speak': 'попросить выступить',
-        'administrator': 'администратор'
-    },
-    'ChannelSlowmode': {
-        5: '5 секунд',
-        10: '10 секунд',
-        15: '15 секунд',
-        30: '30 секунд',
-        60: '1 минута',
-        120: '2 минуты',
-        300: '5 минут',
-        600: '10 минут',
-        900: '15 минут',
-        1800: '30 минут',
-        3600: '1 час',
-        7200: '2 часа',
-        21600: '6 часов'
-    },
-    'BoticordToken': os.environ.get('BoticordToken')
+MongoSettings = {
+    # База данных серверов
+    'GuildsData': MongoClient(f'mongodb+srv://{os.environ.get('MongoKey')}/TestCoffeebase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE').Coffeebase.guilds_data
 }
 
 
-def BotPostfix(num: int, end_1: str='год', end_2: str='года', end_3: str='лет'): # функция постфикса
-    num = num % 10 if num > 20 else num
-    return end_1 if num == 1 else end_2 if 1 < num < 5 else end_3
-
-
-def BotUptime():
-    seconds = 0
-    minutes = 0
-    hours = 0
-    days = 0 # стандартные переменные времени
-
-    while True: # создаём цикл
-        seconds += 1 
-
-        if seconds == 60:
-            minutes += 1
-            seconds = 0
-
-        if minutes == 60:
-            hours += 1
-            minutes = 0
-
-        if hours == 24:
-            days += 1
-            hours = 0
-
-        if seconds != 0:
-            file = open('uptime.txt', 'w')
-            file.write(f'{seconds} {BotPostfix(seconds, "секунда", "секунды", "секунд")}')
-            file.close()
-
-        if minutes != 0:
-            file = open('uptime.txt', 'w')
-            file.write(f'{minutes} {BotPostfix(minutes, "минута", "минуты", "минут")} {seconds} {BotPostfix(seconds, "секунда", "секунды", "секунд")}')
-            file.close()
-
-        if hours != 0:
-            file = open('uptime.txt', 'w')
-            file.write(f'{hours} {BotPostfix(hours, "час", "часа", "часов")} {minutes} {BotPostfix(minutes, "минута", "минуты", "минут")} {seconds} {BotPostfix(seconds, "секунда", "секунды", "секунд")}')
-            file.close()
-
-        if days != 0:
-            file = open('uptime.txt', 'w')
-            file.write(f'{days} {BotPostfix(days, "день", "дня", "дней")} {hours} {BotPostfix(hours, "час", "часа", "часов")} {minutes} {BotPostfix(minutes, "минута", "минуты", "минут")} {seconds} {BotPostfix(seconds, "секунда", "секунды", "секунд")}')
-            file.close()
-        time.sleep(1)
-        
-        
-def GetPrefix(Bot, message):
-    collection = BotSettings['Mongo']['Collection'].custom_prefix
-
-    if collection.count_documents({'guild_id': message.guild.id}) == 0: # проверка на присутствие сервера в базе
-        collection.insert_one({
-            'guild_name': message.guild.name,
-            'guild_id': message.guild.id,
-            'guild_owner_name': f'{message.guild.owner.name}#{message.guild.owner.discriminator}',
-            'guild_owner_id': message.guild.owner.id,
-            'guild_prefix': BotSettings['Bot']['MainPrefix']
-            })
-        
-        prefix = collection.find_one({'guild_id': message.guild.id})
-        return prefix['guild_prefix']
-    else:
-        prefix = collection.find_one({'guild_id': message.guild.id}) # если есть то берем его оттуда
-        return prefix['guild_prefix']
+BotSettings = {
+    # Основной префикс бота
+    'MainPrefix': 'cb.',
+    # Токен бота
+    'Token': os.environ.get('TokenKey'),
+    # Цвет эмбеда в обычных ситуациях
+    'OrangeColor': 0xffa500,
+    # Цвет эмбеда в ошибочных ситуациях
+    'RedColor': 0xe32636,
+    # Цвет эмбеда в успешных ситуациях
+    'GreenColor': 0x90ee90,
+    # Версия бота
+    'BotVersion': 'v1.4',
+    # Ссылка на сервер поддержки
+    'SupportGuild': 'https://discord.gg/7Ja4JNcqUB',
+    # Ссылка на приглашение бота
+    'BotInvite': 'https://discord.com/api/oauth2/authorize?client_id=875927971649712148&permissions=268528647&scope=bot',
+    # Канал аудита неизвестных ошибок бота
+    'ErrorsLogChannel': 896215788455886849,
+    # Канал аудита серверов бота
+    'GuildsLogChannel': 896215771347320872,
+    # Канал аудита личных сообщений бота
+    'PrivateMessagesLogChannel': 896546624615088130,
+    # Канал аудита использованных команд бота
+    'CommandsLogChannel': 896546682068693002
+}
