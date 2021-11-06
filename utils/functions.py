64eaa57f153f99c82f911cde4af118c5e7c44370
Sha4ek <69022471+Sha4ek@users.noreply.github.com>
2021-11-06 16:57:14 +1000

    Update functions.py

--- utils/functions.py
+++ utils/functions.py
@@ -1,26 +1,21 @@
 from disnake.ext import commands
-from utils.config import MongoSettings, BotSettings
+from utils.config import BotConfig, MongoConfig
 
 
-def BotPrefix(Bot, message):
-    """Взятие префикса бота из базы данных"""
+def BotPrefix(bot, message):
+    if not message.guild:
+        return
 
-    GuildsData = MongoSettings['GuildsData'] # переменная с дата-базой монги
-    MainPrefix = BotSettings['MainPrefix'] # переменная основного префикса
+    elif 'GuildPrefix' not in MongoConfig['GuildsData'].find_one({'GuildID': message.guild.id}):
+        prefix = BotConfig['MainPrefix']
+    
+    else:
+        prefix = MongoConfig['GuildsData'].find_one({'GuildID': message.guild.id})['GuildPrefix']
 
-    if GuildsData.count_documents({'GuildID': message.guild.id}) == 0:
-        GuildsData.insert_one({
-            'GuildID': message.guild.id,
-            'GuildPrefix': MainPrefix
-            })
-
-    GetPrefix = GuildsData.find_one({'GuildID': message.guild.id})
-
-    return commands.when_mentioned_or(GetPrefix['GuildPrefix'])(Bot, message)
+    return commands.when_mentioned_or(prefix)(bot, message)
 
 
-def BotPostfix(number: int, first_ending: str = 'день', second_ending: str = 'дня', third_ending: str = 'дней'):
-    """Добавление постфикса к числам"""
-
+def BotPostfix(number: int, first_ending: str='час', second_ending: str='часа', third_ending: str='часов'):
     number = number % 10 if number > 20 else number
+
     return first_ending if number == 1 else second_ending if 1 < number < 5 else third_ending
