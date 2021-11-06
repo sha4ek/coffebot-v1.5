3c6e2daa96f267819f1235bb1b121a0f7b8fd1b5
Sha4ek <69022471+Sha4ek@users.noreply.github.com>
2021-11-06 17:06:08 +1000

    Update help.py

--- modules/help.py
+++ modules/help.py
@@ -1,82 +1,68 @@
-import disnake
+import disnake as discord
 from disnake.ext import commands
 from CoffeePaginator import Paginator
-
-from utils.config import BotSettings
-
-
-OrangeColor = BotSettings['OrangeColor'] # переменная с цветом эмбеда
+from utils.config import BotConfig
+from utils.functions import BotPrefix
 
 
 class Help(commands.Cog):
-    def __init__(self, Bot):
-        self.Bot = Bot
+    def __init__(self, bot):
+        self.bot = bot
 
 
     @commands.command()
     @commands.cooldown(1, 2.0, commands.BucketType.user)
     @commands.bot_has_permissions(send_messages=True, embed_links=True)
     async def help(self, ctx):
-        emb = disnake.Embed(
-            title='Общее:',
-            description=f'> **{ctx.prefix}avatar [участник]** - аватарка учасника\n'
-                        f'> **{ctx.prefix}links** - полезные ссылки',
-            color=OrangeColor
-        )
+        prefix = BotPrefix(self.bot, ctx.message)[2]
 
-        emb1 = disnake.Embed(
-            title='Весёлое:',
-            description=f'> **{ctx.prefix}activity [функция]** - активности в голосовом канале\n'
-                        f'> **{ctx.prefix}kiss <участник>** - реакция "Поцеловать"\n'
-                        f'> **{ctx.prefix}hug <участник>** - реакция "Обнять"\n'
-                        f'> **{ctx.prefix}slap <участник>** - реакция "Пощёчина"\n'
-                        f'> **{ctx.prefix}pat <участник>** - реакция "Погладить"\n',
-            color=OrangeColor
-        )
+        emb = discord.Embed(title='Общее:',
+            description=f'> **{prefix}avatar [участник]** - аватар учасника\n'
+                        f'> **{prefix}invite** - приглашение бота\n'
+                        f'> **{prefix}last-updates** - список последних обновлений бота',
+            color=BotConfig['OrangeColor'])
 
-        emb2 = disnake.Embed(
-            title='Информация:',
-            description=f'> **{ctx.prefix}stats** - немного статистики бота\n'
-                        f'> **{ctx.prefix}user [участник]** - информация об участнике\n'
-                        f'> **{ctx.prefix}server** - информация о сервере\n'
-                        f'> **{ctx.prefix}channel [канал]** - информация о канале',
-            color=OrangeColor
-        )
+        emb1 = discord.Embed(title='Весёлое:',
+            description=f'> **{prefix}activity <youtube | chess | poker>** - активности в голосовом канале\n'
+                        f'> **{prefix}kiss <участник>** - реакция "поцеловать"\n'
+                        f'> **{prefix}hug <участник>** - реакция "обнять"\n'
+                        f'> **{prefix}slap <участник>** - реакция "дать пощёчину"\n'
+                        f'> **{prefix}pat <участник>** - реакция "погладить"\n',
+            color=BotConfig['OrangeColor'])
 
-        emb3 = disnake.Embed(
-            title='Модерация:',
-            description=f'> **{ctx.prefix}clear <количество сообщений>** - очистка чата\n'
-                        f'> **{ctx.prefix}addrole <участник> <роль>** - добавление роли участнику\n'
-                        f'> **{ctx.prefix}ban <участник> [причина]** - бан участника\n'
-                        f'> **{ctx.prefix}kick <участник> [причина]** - кик участника\n'
-                        f'> **{ctx.prefix}unban <участник>** - разбан участника',
-            color=OrangeColor
-        )
+        emb2 = discord.Embed(title='Информация:',
+            description=f'> **{prefix}bot-stats** - статистика бота\n'
+                        f'> **{prefix}user-info [участник]** - информация об участнике\n'
+                        f'> **{prefix}guild-info** - информация о сервере\n'
+                        f'> **{prefix}channel-info [канал]** - информация о канале',
+            color=BotConfig['OrangeColor'])
 
-        emb4 = disnake.Embed(
-            title='Настройки:',
-            description=f'> **{ctx.prefix}prefix <префикс>** - установка префикса для сервера',
-            color=OrangeColor
-        )
+        emb3 = discord.Embed(title='Модерация:',
+            description=f'> **{prefix}clear <количество сообщений>** - очистка чата\n'
+                        f'> **{prefix}ban <участник> [причина]** - бан участника\n'
+                        f'> **{prefix}kick <участник> [причина]** - кик участника\n'
+                        f'> **{prefix}unban <участник>** - разбан участника',
+            color=BotConfig['OrangeColor'])
 
-        emb5 = disnake.Embed(
-            title='Для разработчика:',
-            description=f'> **{ctx.prefix}modules [функция] [модуль]** - управление модулями бота',
-            color=OrangeColor
-        )
+        emb4 = discord.Embed(title='Настройки:',
+            description=f'> **{prefix}set-prefix <standard | префикс>** - установка префикса для сервера',
+            color=BotConfig['OrangeColor'])
+
+        emb5 = discord.Embed(title='Для разработчика:',
+            description=f'> **{prefix}modules <load | unload | reload> [модуль]** - управление модулями бота\n'
+                        f'> **{prefix}eval <код>** - выполнение кода вне редактора',
+            color=BotConfig['OrangeColor'])
         
         embs = [emb, emb1, emb2, emb3, emb4]
-        message = await ctx.send(embed=emb)
         
-        if ctx.author.id != 546502974499717122:
-            embs = embs
-        else:
+        if ctx.author.id == BotConfig['DeveloperID']:
             embs.append(emb5)
-        
+
+        message = await ctx.send(embed=emb)
         pages = Paginator(message, embs, ctx.author, True, 60)
         await pages.start()
 
 
-def setup(Bot):
-    Bot.add_cog(Help(Bot))
-    print(f'[MODULES] Module "Help" is loaded!')
+def setup(bot):
+    bot.add_cog(Help(bot))
+    print(f'[SYSTEM] Module "help" loaded!')
