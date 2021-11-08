2a26ff78f5301c4e153f0dbac4eb5950998b0bae
Sha4ek <69022471+Sha4ek@users.noreply.github.com>
2021-11-08 16:47:57 +1000

    Update monitorings.py

--- modules/monitorings.py
+++ modules/monitorings.py
@@ -3,27 +3,26 @@ from disnake.ext import commands, tasks
 from utils.config import BotConfig
 
 class Monitorings(commands.Cog):
-    async def __init__(self, bot):
+    def __init__(self, bot):
         self.bot = bot
         self.blist = blist.Blist(self.bot, token=BotConfig['Blist'])
-        self.webhook = blist.WebhookServer(self.blist)
-
-        await self.webhook.run()
+        self.blisthook = blist.WebhookServer(self.blist)
 
     
     @tasks.loop(seconds=120)
     async def blist_post(self):
         await self.blist.post_bot_stats()
 
-        # Outside of a cog
-
-    @commands.Cog.listener()
-    async def on_blist_vote(self, user_id, time):
-        pass
     
     @commands.Cog.listener()
     async def on_ready(self):
         self.blist_post.start()
+        await self.blisthook.run()
+        
+    
+    @commands.Cog.listener()
+    async def on_blist_vote(self, user_id, time):
+        pass
 
     
 def setup(bot):
