14d6e46cb69a5e86b15d4da7299edf34c85d322c
Sha4ek <69022471+Sha4ek@users.noreply.github.com>
2021-12-17 20:12:46 +1000

    Update develop.py

--- modules/develop.py
+++ modules/develop.py
@@ -13,8 +13,8 @@ class Develop(commands.Cog):
     @commands.command()
     @commands.is_owner()
     async def eval(self, ctx, *, ucode=None):
-        view = disnake.ui.View()
-        view.add_item(disnake.ui.Button(label='Перепригласить', url=BotConfig['BotInvite']))
+        view = discord.ui.View()
+        view.add_item(discord.ui.Button(label='Перепригласить', url=BotConfig['BotInvite']))
         
         if not ucode:
             emb = discord.Embed(title='Ошибка:',
